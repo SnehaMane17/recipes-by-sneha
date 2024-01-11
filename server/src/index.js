35c00b9f165b50fc3d5ee50c480d83728775688a
SnehaMane17 <snehamane171998@gmail.com>
2024-01-11 20:35:32 +0530

    user model added

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,16 @@
 import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
+
+mongoose.connect(process.env.URL)
+.then(()=> console.log('connected to db'))
+.catch(()=> console.log('Error connecting db'))
 
 app.listen(3000, console.log('Server started'))
